ca697edb68d96222444c28e9602fd20d82d7b284
LiK-h1n <likh1n709@gmail.com>
2023-06-02 03:00:17 -0700

    Added contact page widgets

--- pages/Contact_Me.py
+++ pages/Contact_Me.py
@@ -1,3 +1,8 @@
-from streamlit import header
+from streamlit import header, form, form_submit_button, text_input, text_area
 
 header("Contact Me")
+
+with form("Send Email", True):
+    user_email = text_input("Your email address")
+    message = text_area("Your message")
+    form_submit_button("SUBMIT")
