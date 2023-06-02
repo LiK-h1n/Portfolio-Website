b79c55d76f390c7a1d7413e5063483de42744dd2
LiK-h1n <likh1n709@gmail.com>
2023-06-02 02:29:06 -0700

    Added project titles

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
-from streamlit import image, columns, title, write, set_page_config
+from streamlit import image, columns, title, write, set_page_config, header
+from pandas import read_csv
 
 set_page_config(layout="wide")
 
 col_1, col_2 = columns(2)
-
 with col_1:
-    image("images/photo.jfif")
+    image("images/photo.jfif", width=300)
 with col_2:
     title("Nikhil Idiculla")
-    content = "Passionate Python programmer with a drive to create innovative solutions and explore the endless " \
-              "possibilities of code, fueled by a deep curiosity and a love for elegant problem-solving."
-    write(content)
+    content_1 = "Passionate Python programmer with a drive to create innovative solutions and explore the endless " \
+                "possibilities of code, fueled by a deep curiosity and a love for elegant problem-solving."
+    write(content_1)
+
+content_2 = "Below you can find some of the apps I have built in Python. Feel free to contact me"
+write(content_2)
+
+col_3, col_4 = columns(2)
+df = read_csv("data.csv", sep=";")
+for index, row in df.iterrows():
+    if index % 2 == 0:
+        with col_3:
+            header(row["title"])
+    else:
+        with col_4:
+            header(row["title"])
